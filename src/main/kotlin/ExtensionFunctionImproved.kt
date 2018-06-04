b89c2eb58ffe2d2c4de3dcd57dfed56f99f7db7c
Juan Ignacio Vimberg <juan.vimberg@oracle.com>
2018-06-04 07:48:54 -0700

    Adding worst version of Extension functions

--- src/main/kotlin/ExtensionFunctionImproved.kt
+++ src/main/kotlin/ExtensionFunctionImproved.kt
@@ -1,6 +1,4 @@
-fun String.countWhitespaces(): Int {
-    return this.filter { it == ' ' }.count()
-}
+//fun String.countWhitespaces(): Int = count { it == ' ' }
 
 fun main(args: Array<String>) {
     print("There are 6 whitespaces in this sentence".countWhitespaces())
