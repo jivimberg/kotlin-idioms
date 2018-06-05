b63659a45defb86973f0d1f1ea6c6bee4c05e2a7
Juan Ignacio Vimberg <juan.vimberg@oracle.com>
2018-06-05 01:11:22 -0700

    if expression

---
+++ src/main/java/IfExpression.java
@@ -0,0 +1,13 @@
+public class IfExpression {
+
+    public void ifExpression(String input) {
+        int i;
+        if(input.equals("one")) {
+            i = 1;
+        } else if(input.equals("two")) {
+            i = 2;
+        } else {
+            i = 3;
+        }
+    }
+}
---
+++ src/main/kotlin/ifExpression.kt
@@ -0,0 +1,9 @@
+fun ifExpression(input: String) {
+    val i = if (input == "one") {
+        1
+    } else if (input == "two") {
+        2
+    } else {
+        3
+    }
+}
